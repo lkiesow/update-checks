2f80ee011c0ab3627b38d84954e26d339d6a88c6
Lars Kiesow <lkiesow@uos.de>
2018-12-08 21:12:32 +0100

    Send mail

---
+++ send-mail.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+set -ue
+
+FROM=no-reply@lkiesow.de
+SUBJECT='Update Notification'
+TO=update-notification@lkiesow.de
+
+[ -f update.log ] || exit
+
+payload='{
+	"personalizations": [
+		{
+			"to": [
+				{"email": null}
+			]
+		}],
+	"from": {"email": null},
+	"subject": null,
+	"content": [
+		{
+			"type": "text/plain",
+			"value": null
+		}]
+	}'
+
+payload="$(echo "$payload" |
+    jq --arg key "$FROM" ".from.email = \$key" |
+    jq --arg key "$TO" ".personalizations[].to[].email = \$key" |
+    jq --arg key "$SUBJECT" ".subject = \$key" |
+	 jq --arg key "$(cat update.log)" ".content[].value = \$key"
+	 )"
+
+echo "$payload" | jq
+
+curl -f --request POST \
+  --url https://api.sendgrid.com/v3/mail/send \
+  --header "Authorization: Bearer $SENDGRID_API_KEY" \
+  --header 'Content-Type: application/json' \
+  --data "${payload}"
