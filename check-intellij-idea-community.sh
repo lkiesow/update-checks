24c4c0a877ddee14bef4c40816f85c2b83d9e308
Lars Kiesow <lkiesow@uos.de>
2018-12-08 23:30:44 +0100

    list build number

--- check-intellij-idea-community.sh
+++ check-intellij-idea-community.sh
@@ -2,14 +2,19 @@
 
 LATEST="$(
 	curl -s 'https://data.services.jetbrains.com/products/releases?code=IIC&latest=true&type=release' \
-		| jq -r '.IIC[0].version'
 	)"
 
+version="$(echo "$LATEST" | jq -r '.IIC[0].version')"
+build="$(echo "$LATEST" | jq -r '.IIC[0].build')"
+LATEST="$version ($build)"
+
 PKG="$(
-	curl -s 'https://raw.githubusercontent.com/lkiesow/intellij-idea-community-rpm/master/intellij-idea-community.spec' \
-		| grep Version: \
-		| awk '{print $2}'
+	curl -s 'https://raw.githubusercontent.com/lkiesow/intellij-idea-community-rpm/master/intellij-idea-community.spec'
 	)"
+
+version="$(echo "$PKG" | grep Version: | awk '{print $2}')"
+build="$(echo "$PKG" | grep '%global build_vers' | awk '{print $3}')"
+PKG="$version ($build)"
 
 if [ "$LATEST" != "$PKG" ]; then
 	printf '%-32s %s -> %s\n' 'intellij-idea-community' "$PKG" "$LATEST" >> update.log
