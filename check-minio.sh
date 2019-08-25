32d39e7057c68620013896b968ce0c0af40fc65f
Lars Kiesow <lkiesow@uos.de>
2019-08-25 21:31:18 +0200

    Check minio

--- check-minio.sh
+++ check-minio.sh
@@ -1,19 +1,22 @@
 #!/bin/sh
 
+echo "Checking minio"
+
 LATEST="$(
 	curl -s 'https://github.com/minio/minio/releases' \
 		| sed -n 's_^.*href="/minio/minio/tree/\(RELEASE\.[^"]*\)".*$_\1_p' \
 		| head -n1
 	)"
 
-echo $LATEST
-exit
+echo "Latest version:  $LATEST"
 
 PKG="$(
-	curl -s 'https://copr-be.cloud.fedoraproject.org/results/lkiesow/gron/fedora-28-x86_64/00782488-gron/gron.spec' \
-		| grep Version: \
-		| awk '{print $2}'
+	curl -s 'https://raw.githubusercontent.com/lkiesow/minio-rpm/master/minio.spec' \
+		| grep '%define *tag' \
+		| awk '{print $3}'
 	)"
+
+echo "Package version: $PKG"
 
 if [ "$LATEST" != "$PKG" ]; then
 	printf '%-32s %s -> %s\n' 'gron' "$PKG" "$LATEST" >> update.log
