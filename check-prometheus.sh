9da878e80e6ad6e365c1327c1762a0d320be6fa3
Lars Kiesow <lkiesow@uos.de>
2020-10-12 16:23:48 +0200

    Check Prometheus

---
+++ check-prometheus.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+echo "Checking Prometheus"
+
+LATEST="$(
+	curl -s 'https://github.com/prometheus/prometheus/releases' \
+		| sed -n 's_^.*href="/prometheus/prometheus/tree/v\([^"-]*\)".*$_\1_p' \
+		| head -n1
+	)"
+
+echo "Latest version:  $LATEST"
+
+PKG="$(
+	curl -s 'https://raw.githubusercontent.com/lkiesow/prometheus-rpm/master/prometheus.spec' \
+		| grep Version: \
+		| awk '{print $2}'
+	)"
+
+echo "Package version: $PKG"
+
+if [ "$LATEST" != "$PKG" ]; then
+	printf '%-32s %s -> %s\n' 'nexus-oss' "$PKG" "$LATEST" >> update.log
+fi
